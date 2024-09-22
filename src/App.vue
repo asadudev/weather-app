d6acf9d46335ae8d00145e8a7dc2db4c9575ddae
Asad Ullah <assadudev@gmail.com>
2024-09-22 19:15:56 +0500

    create a setup for weather App

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <SiteNavigation/>
+    <RouterView/>
+  </div>
+</template>
+<script setup>
+import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
+</script>
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    this is the home page 
+  </div>
+</template>
+<script setup>
+</script>
+<style>
+</style>
